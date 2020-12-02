1e0416415a40ba7cb8efbbaa8b36e6eefb9532d6
gn01689576 <70142830+gn01689576@users.noreply.github.com>
2020-12-02 20:20:27 +0800

    敵人移動

---
+++ EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public GameObject explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += new Vector3(0, -0.01f, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Ship" || col.tag == "Bullet") //如果碰撞的標籤是Ship或Bullet
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(col.gameObject); //消滅碰撞的物件
+            Destroy(gameObject); //消滅物件本身
+            if (col.tag == "Ship")
+            {
+                Instantiate(explosion, transform.position, transform.rotation);
+                GameObject.Find("BackGround").GetComponent<GameCtrl>().GameOver();
+            }
+        }
+    }
+}
